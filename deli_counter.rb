e524dac413387e0e49c7f07e58f35dbbb721939f
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-17 04:22:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,16 @@
 katz_deli = []
 
-    def line(katz_deli)
-      if katz_deli.count == 0
-        puts "The line is currently empty."
-      else
-        counter = 2
-        position = 1
-        while position < katz_deli.count
-          katz_deli.insert(position, " #{counter}. ")
-          counter +=1
-          position +=2
-        end
-        puts "The line is currently: 1. #{katz_deli.join}"
-      end
+def line(katz_deli)
+  if katz_deli.count == 0
+    puts "The line is currently empty."
+  else
+    counter = 2
+    position = 1
+    while position < katz_deli.count
+      katz_deli.insert(position, " #{counter}. ")
+      counter +=1
+      position +=2
     end
+    puts "The line is currently: 1. #{katz_deli.join}"
+  end
+end
