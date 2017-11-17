3d761af63eeaa8891057895167e316cf98b449fb
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-17 03:44:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,3 +10,19 @@ def line(katz_deli)
 
   end
 end
+
+    puts "The line is currently: 1. #{katz_deli.join(" #{katz_deli.index}. ")}"
+
+    def line(katz_deli)
+      if katz_deli.count == 0
+        puts "The line is currently empty."
+      else
+        counter = 2
+        position = 0
+        while counter < (index_total) 
+          katz_deli.insert((position+2), (counter))
+          counter +=2          
+        end
+        puts "The line is currently: 1. #{katz_deli.join}
+      end
+    end
