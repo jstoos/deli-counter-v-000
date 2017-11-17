1133a1a8f48c63102f4ef61deb920a7fcabc3a12
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-17 04:55:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.count ==0
-    puts
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli[0]}."
     katz_deli.delete_at(0)
