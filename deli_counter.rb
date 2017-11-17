c3f38ca4e09a29a82d076f60bb8e41d5673e3294
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-17 04:51:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,3 +19,8 @@ def take_a_number(katz_deli, name)
   katz_deli = katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.count !=0
+    puts katz_deli[0]
+    katz_deli.delete_at(0)
