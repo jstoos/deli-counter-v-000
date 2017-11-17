a36aa6f044bfbd79693ab59b156f8db2fc922b7b
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-17 03:51:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ katz_deli = []
         counter == 2
         position == 0
         while counter < (katz_deli.count)
-          katz_deli.insert((position+2), "#{counter}. "
+          katz_deli.insert((position+2), "#{counter}. ")
           counter +=2
         end
         puts "The line is currently: 1. #{katz_deli.join}
