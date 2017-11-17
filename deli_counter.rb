a37a71fee23102bc21115f133eb9808ffb0821ce
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-17 03:53:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ katz_deli = []
       if katz_deli.count == 0
         puts "The line is currently empty."
       else
-        counter == 2
-        position == 0
+        counter = 2
+        position = 0
         while counter < (katz_deli.count)
           katz_deli.insert((position+2), "#{counter}. ")
           counter +=2
