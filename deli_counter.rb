51915fb2a2e8539867fdc50df4f00d56bf810891
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-17 03:07:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.count == 0
+    puts "The line is currently empty."
+  else
+    counter = 1
+    puts "The line is currently: 1. #{katz_deli.join((counter)". , ")}"
+      counter +=1
+
+  end
+end
