e2b2fd1e03976f30e8cf749f6639fb299d89e1b2
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-17 03:44:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,27 +1,14 @@
 katz_deli = []
 
-def line(katz_deli)
-  if katz_deli.count == 0
-    puts "The line is currently empty."
-  else
-    counter = 2
-    puts "The line is currently: 1. #{katz_deli.join(" #{katz_deli.index}. ")}"
-      counter +=1
-
-  end
-end
-
-    puts "The line is currently: 1. #{katz_deli.join(" #{katz_deli.index}. ")}"
-
     def line(katz_deli)
       if katz_deli.count == 0
         puts "The line is currently empty."
       else
         counter = 2
         position = 0
-        while counter < (index_total) 
+        while counter < (index_total)
           katz_deli.insert((position+2), (counter))
-          counter +=2          
+          counter +=2
         end
         puts "The line is currently: 1. #{katz_deli.join}
       end
